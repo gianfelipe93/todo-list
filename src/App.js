88d33495c0f8f4a8d15c60e3f61f614cebcbe965
Gian Felipe <gianfelipe@Gians-MacBook-Pro.local>
2018-03-31 18:50:09 +1100

    Header, input with button and todolist component created

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import TodoList from './TodoList'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="container">
+        <div className="text-center">
+          <h1>
+            Todos
+          </h1>
+        </div>
+
+        <div className="input-group mb-3">
+          <input type="text" className="form-control" placeholder="new todo description..." />
+          <div className="input-group-append">
+            <button className="btn btn-success" type="button">Add</button>
+          </div>
+        </div>
+
+        <TodoList />
       </div>
     );
   }
